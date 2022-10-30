1b2022ef17663c130728f81c67464026e2f3adc4
Atharva Pardeshi <atharva.exe@gmail.com>
2022-10-30 22:58:44 +0530

    feat: wrap app with container

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import { Box, Container } from '@chakra-ui/react'
+
 const Page = () => {
-  return <div>Hello</div>
+  return (
+    <Container>
+      <Box borderRadius={'lg'} bg="red" p={3} textAlign="center">
+        Hello, I&apos;m a full-stack developer based in India!
+      </Box>
+    </Container>
+  )
 }
 
 export default Page
