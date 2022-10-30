dfe49341ef51f2cfd15887172eede7a997acd35e
Atharva Pardeshi <atharva.exe@gmail.com>
2022-10-30 23:05:53 +0530

    feat: add title and small bio

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
-import { Box, Container } from '@chakra-ui/react'
+import { Box, Container, Heading } from '@chakra-ui/react'
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius={'lg'} bg="red" p={3} textAlign="center">
+      <Box borderRadius={'lg'} bg="red" p={3} mb={6} textAlign="center">
         Hello, I&apos;m a full-stack developer based in India!
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as={'h2'} variant="page-title">
+            Atharva Pardeshi
+          </Heading>
+          <p>Digital Craftsman ( Artist / Developer / Designer )</p>
+        </Box>
       </Box>
     </Container>
   )
