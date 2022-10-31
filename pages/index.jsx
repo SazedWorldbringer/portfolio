70c9d7178538fec7afe9db1b41cdb792d95a7ae6
Atharva Pardeshi <atharva.exe@gmail.com>
2022-11-01 00:51:46 +0530

    feat: add profile image, change bg of bio box

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,21 @@
-import { Box, Container, Heading } from '@chakra-ui/react'
+import {
+  Box,
+  Container,
+  Heading,
+  Image,
+  useColorModeValue
+} from '@chakra-ui/react'
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius={'lg'} bg="red" p={3} mb={6} textAlign="center">
+      <Box
+        borderRadius={'lg'}
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3}
+        mb={6}
+        textAlign="center"
+      >
         Hello, I&apos;m a full-stack developer based in India!
       </Box>
 
@@ -13,6 +25,23 @@ const Page = () => {
             Atharva Pardeshi
           </Heading>
           <p>Digital Craftsman ( Artist / Developer / Designer )</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign={'center'}
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/profile.png"
+            alt="Profile image"
+          />
         </Box>
       </Box>
     </Container>
