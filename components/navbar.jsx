1f570cdc816e2be2034bee2bd06e2fb8f2c8af4d
Atharva Pardeshi <atharva.exe@gmail.com>
2022-10-31 17:32:20 +0530

    feat: import and render theme toggle button

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -15,6 +15,7 @@ import {
   useColorModeValue
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
+import ThemeToggleButton from './theme-toggle-button'
 
 const LinkItem = ({ href, path, children }) => {
   const active = path === href
@@ -78,6 +79,7 @@ const Navbar = props => {
         </Stack>
 
         <Box flex={1} textAlign="right">
+          <ThemeToggleButton />
           <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
             <Menu>
               <MenuButton
