e41d71ac000bbd901b08dcc79f1216821ee96f2c
Atharva Pardeshi <atharva.exe@gmail.com>
2022-11-01 00:23:58 +0530

    feat: add tooltip for theme button

--- components/theme-toggle-button.jsx
+++ components/theme-toggle-button.jsx
@@ -1,16 +1,30 @@
-import { IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react'
+import {
+  IconButton,
+  Tooltip,
+  useColorMode,
+  useColorModeValue
+} from '@chakra-ui/react'
 import { SunIcon, MoonIcon } from '@chakra-ui/icons'
 
 const ThemeToggleButton = () => {
   const { toggleColorMode } = useColorMode()
 
   return (
-    <IconButton
-      aria-label="Toggle theme"
-      colorScheme={useColorModeValue('purple', 'orange')}
-      icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
-      onClick={toggleColorMode}
-    />
+    <Tooltip
+      hasArrow
+      label="Change theme"
+      openDelay={300}
+      closeDelay={300}
+      bg={useColorModeValue('purple.400', 'orange.200')}
+      rounded="lg"
+    >
+      <IconButton
+        aria-label="Toggle theme"
+        colorScheme={useColorModeValue('purple', 'orange')}
+        icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
+        onClick={toggleColorMode}
+      />
+    </Tooltip>
   )
 }
 
