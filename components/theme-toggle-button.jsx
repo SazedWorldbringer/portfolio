42fbfbc765d72a5d516ed72a737500ca627c1862
Atharva Pardeshi <atharva.exe@gmail.com>
2022-11-01 22:32:53 +0530

    feat: remove tooltip, increase animation duration

--- components/theme-toggle-button.jsx
+++ components/theme-toggle-button.jsx
@@ -1,10 +1,5 @@
 import { AnimatePresence, motion } from 'framer-motion'
-import {
-  IconButton,
-  Tooltip,
-  useColorMode,
-  useColorModeValue
-} from '@chakra-ui/react'
+import { IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import { SunIcon, MoonIcon } from '@chakra-ui/icons'
 
 const ThemeToggleButton = () => {
@@ -12,30 +7,21 @@ const ThemeToggleButton = () => {
 
   return (
     <AnimatePresence exitBeforeEnter initial={false}>
-      <Tooltip
-        hasArrow
-        label="Change theme"
-        openDelay={300}
-        closeDelay={300}
-        bg={useColorModeValue('purple.400', 'orange.200')}
-        rounded="lg"
+      <motion.div
+        style={{ display: 'inline-block' }}
+        key={useColorModeValue('light', 'dark')}
+        initial={{ y: -20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 20, opacity: 0 }}
+        transition={{ duration: 0.3 }}
       >
-        <motion.div
-          style={{ display: 'inline-block' }}
-          key={useColorModeValue('light', 'dark')}
-          initial={{ y: -20, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          exit={{ y: 20, opacity: 0 }}
-          transition={{ duration: 0.2 }}
-        >
-          <IconButton
-            aria-label="Toggle theme"
-            colorScheme={useColorModeValue('purple', 'orange')}
-            icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
-            onClick={toggleColorMode}
-          />
-        </motion.div>
-      </Tooltip>
+        <IconButton
+          aria-label="Toggle theme"
+          colorScheme={useColorModeValue('purple', 'orange')}
+          icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
+          onClick={toggleColorMode}
+        />
+      </motion.div>
     </AnimatePresence>
   )
 }
