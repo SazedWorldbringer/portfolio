e05fc2f1a6dec6b309eae7c274594fca98f33a17
Atharva Pardeshi <atharva.exe@gmail.com>
2022-11-01 14:54:22 +0530

    feat: import more fonts

--- components/fonts.jsx
+++ components/fonts.jsx
@@ -3,7 +3,7 @@ import { Global } from '@emotion/react'
 const Fonts = () => (
   <Global
     styles={`
-    @import url("https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap");
+      @import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&family=Roboto:wght@300;400;700&family=Space+Mono:ital,wght@0,700;1,400&display=swap');
     `}
   />
 )
