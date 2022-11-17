77140c17cbcdcabe8c5e4be347c2817039855c2f
Atharva Pardeshi <atharva.exe@gmail.com>
2022-11-17 23:19:27 +0530

    fix: typo

--- components/voxel-dog.js
+++ components/voxel-dog.js
@@ -125,7 +125,7 @@ const VoxelDog = () => {
       ref={refContainer}
       className="voxel-dog"
       m="auto"
-      at={['-20px', '-60px', '-120px']}
+      mt={['-20px', '-60px', '-120px']}
       mb={['-40px', '-140px', '-200px']}
       w={[280, 480, 640]}
       h={[280, 480, 640]}
