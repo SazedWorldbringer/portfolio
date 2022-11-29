93491cf3f44d0d55bdf6d8077ba7a1166543ea87
Atharva Pardeshi <atharva.exe@gmail.com>
2022-11-29 12:25:43 +0530

    feat: add jammming and cli todo to works grid

--- pages/works.jsx
+++ pages/works.jsx
@@ -3,7 +3,9 @@ import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 import Layout from '../components/layouts/article'
 
-import thumbnailCoolCards from '../public/images/works/cool-cards.png'
+import thumbCoolCards from '../public/images/works/cool-cards.png'
+import thumbJammming from '../public/images/works/jammming.png'
+import thumbTodo from '../public/images/works/todo.png'
 
 const Works = () => {
   return (
@@ -16,9 +18,29 @@ const Works = () => {
         <SimpleGrid columns={[1, 1, 2]} gap={6}>
           <Section>
             <WorkGridItem
+              id="jammming"
+              title="Jammming"
+              thumbnail={thumbJammming}
+            >
+              An app that lets you search songs and create playlists easily.
+            </WorkGridItem>
+          </Section>
+
+          <Section>
+            <WorkGridItem
+              id="basictodo"
+              title="Basic CLI Todo"
+              thumbnail={thumbTodo}
+            >
+              Todo list for my terminal buddies.
+            </WorkGridItem>
+          </Section>
+
+          <Section>
+            <WorkGridItem
               id="coolcard"
               title="Card Hover Effect"
-              thumbnail={thumbnailCoolCards}
+              thumbnail={thumbCoolCards}
             >
               A cool card hover effect written following bedimcode's YouTube
               tutorial.
@@ -29,7 +51,7 @@ const Works = () => {
             <WorkGridItem
               id="coolcard"
               title="Card Hover Effect"
-              thumbnail={thumbnailCoolCards}
+              thumbnail={thumbCoolCards}
             >
               A cool card hover effect written following bedimcode's YouTube
               tutorial.
@@ -40,7 +62,7 @@ const Works = () => {
             <WorkGridItem
               id="coolcard"
               title="Card Hover Effect"
-              thumbnail={thumbnailCoolCards}
+              thumbnail={thumbCoolCards}
             >
               A cool card hover effect written following bedimcode's YouTube
               tutorial.
@@ -51,7 +73,7 @@ const Works = () => {
             <WorkGridItem
               id="coolcard"
               title="Card Hover Effect"
-              thumbnail={thumbnailCoolCards}
+              thumbnail={thumbCoolCards}
             >
               A cool card hover effect written following bedimcode's YouTube
               tutorial.
@@ -62,7 +84,7 @@ const Works = () => {
             <WorkGridItem
               id="coolcard"
               title="Card Hover Effect"
-              thumbnail={thumbnailCoolCards}
+              thumbnail={thumbCoolCards}
             >
               A cool card hover effect written following bedimcode's YouTube
               tutorial.
@@ -73,29 +95,7 @@ const Works = () => {
             <WorkGridItem
               id="coolcard"
               title="Card Hover Effect"
-              thumbnail={thumbnailCoolCards}
-            >
-              A cool card hover effect written following bedimcode's YouTube
-              tutorial.
-            </WorkGridItem>
-          </Section>
-
-          <Section>
-            <WorkGridItem
-              id="coolcard"
-              title="Card Hover Effect"
-              thumbnail={thumbnailCoolCards}
-            >
-              A cool card hover effect written following bedimcode's YouTube
-              tutorial.
-            </WorkGridItem>
-          </Section>
-
-          <Section>
-            <WorkGridItem
-              id="coolcard"
-              title="Card Hover Effect"
-              thumbnail={thumbnailCoolCards}
+              thumbnail={thumbCoolCards}
             >
               A cool card hover effect written following bedimcode's YouTube
               tutorial.
